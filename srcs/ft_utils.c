c87acc7ab0dfb88224bcb31f0f6bba6b1878a462
brandonlooksdumb <deustch.private@gmail.com>
2022-12-03 17:27:11 +0100

    :hammer: Added utils

---
+++ srcs/ft_utils.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsoubaig <bsoubaig@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/03 17:23:48 by bsoubaig          #+#    #+#             */
+/*   Updated: 2022/12/03 17:26:52 by bsoubaig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/***
+ * A atol function
+*/
+
+/***
+ * a function to free all stacks
+ * it should take two pointers to t_stack
+ * and free their stack and the t_stack itself
+*/
+
+/***
+ * a ft_error function that takes a char **, a t_stack *, and an int *
+ * it should free all the pointers it receives if they are not NULL
+ * and then write "Error" to the standard error
+ * and exit the program with EXIT_FAILURE
+*/
