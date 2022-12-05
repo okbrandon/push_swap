e59968c1f891fe1c19db97b280c5b51b0a19a6aa
Brandon Soubaigne <bsoubaig@student.42nice.fr>
2022-12-05 19:28:38 +0100

    :rocket: Completed args process functions

--- srcs/ft_args_process.c
+++ srcs/ft_args_process.c
@@ -3,18 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_args_process.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bsoubaig <bsoubaig@student.42nice.fr>      +#+  +:+       +#+        */
+/*   By: bsoubaig <bsoubaig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 17:47:39 by bsoubaig          #+#    #+#             */
-/*   Updated: 2022/12/03 18:17:19 by bsoubaig         ###   ########.fr       */
+/*   Updated: 2022/12/05 19:28:10 by bsoubaig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "push_swap.h"
 
 /***
  * a free and join function that takes two char * and returns a char *
  * it should strjoin the two strings and free the first one
  * and return the new string
 */
+char	*ft_free_and_join(char *s1, char *s2)
+{
+	char	*temp;
+
+	temp = ft_strjoin(s1, s2);
+	free(s1);
+	return (temp);
+}
 
 /***
  * a function that takes a char * and returns an int
@@ -26,6 +36,30 @@
  *  and increment a counter when it finds a space
  * it should return the counter
 */
+int	ft_int_tab_size(char *s)
+{
+	int	i;
+	int	counter;
+
+	i = 0;
+	counter = 0;
+	while (s[i])
+	{
+		if (s[i] == ' ')
+		{
+			while (s[i] == ' ' && s[i])
+				i++;
+		}
+		else
+		{
+			while (s[i] != ' ' && s[i])
+				i++;
+			counter++;
+		}
+		i++;
+	}
+	return (counter);
+}
 
 /***
  * a function that will make a splitted array of strings 
@@ -40,3 +74,26 @@
  * the char ** should be a splitted array of char * with ' ' using ft_split
  * we should free the char * and return the char **
 */
+char	**ft_args_process(int argc, char **argv, t_stack *stack)
+{
+	int		i;
+	char	*s;
+	char	**splitted;
+
+	i = 1;
+	s = ft_calloc(1, sizeof(char));
+	if (!s)
+		return (NULL);
+	while (i < argc)
+	{
+		s = ft_free_and_join(s, argv[i]);
+		if (i < argc - 1)
+			s = ft_strjoin(s, " ");
+		i++;
+	}
+	stack->size = ft_int_tab_size(s);
+	stack->max_size = ft_int_tab_size(s);
+	splitted = ft_split(s, ' ');
+	free(s);
+	return (splitted);
+}
