be2a1cb56ab1113b10ac53f72ef94abdf9e8468a
brandonlooksdumb <deustch.private@gmail.com>
2022-12-05 19:40:09 +0100

    :hammer: Added clean comments to args process

--- srcs/ft_args_process.c
+++ srcs/ft_args_process.c
@@ -3,20 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_args_process.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bsoubaig <bsoubaig@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bsoubaig <bsoubaig@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 17:47:39 by bsoubaig          #+#    #+#             */
-/*   Updated: 2022/12/05 19:28:10 by bsoubaig         ###   ########.fr       */
+/*   Updated: 2022/12/05 19:39:39 by bsoubaig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/***
- * a free and join function that takes two char * and returns a char *
- * it should strjoin the two strings and free the first one
- * and return the new string
-*/
+/**
+ * @brief A function to re-alloc a string
+ * 
+ * @param s1 			pointer to the string to be re-allocated
+ * @param s2 			pointer to the string to be added to s1
+ * @return char* 		pointer to the new string
+ */
 char	*ft_free_and_join(char *s1, char *s2)
 {
 	char	*temp;
@@ -26,16 +28,12 @@ char	*ft_free_and_join(char *s1, char *s2)
 	return (temp);
 }
 
-/***
- * a function that takes a char * and returns an int
- * it should iterate over the char *
- * if it finds a space it should iterate while it finds a space and 
- *  that it doesn't reach the end of the string
- * else if it finds something else, it should iterate while it doesn't
- *  find a space and that it doesn't reach the end of the string
- *  and increment a counter when it finds a space
- * it should return the counter
-*/
+/**
+ * @brief A function to determine the size to be allocated for the stack
+ * 
+ * @param s 			pointer to the string to be processed
+ * @return int 			the size to be allocated 
+ */
 int	ft_int_tab_size(char *s)
 {
 	int	i;
@@ -61,19 +59,14 @@ int	ft_int_tab_size(char *s)
 	return (counter);
 }
 
-/***
- * a function that will make a splitted array of strings 
- * it should take the number of arguments, the arguments, and a t_stack *
- * it should have an index i, a char * and a char **
- * i will increment from 1 to argc
- * the char * will be a copy of the argument at i using the free and join
- * 	function
- * also if i is not reaching argc - 1 we should add a space to the char *
- * the stack size should be the number of words in the char *
- * the stack max size should be the number of words in the char *
- * the char ** should be a splitted array of char * with ' ' using ft_split
- * we should free the char * and return the char **
-*/
+/**
+ * @brief A function to process the arguments
+ * 
+ * @param argc 			the number of arguments
+ * @param argv 			pointer to the arguments
+ * @param stack 		pointer to the stack to be filled
+ * @return char** 		pointer to the array of strings
+ */
 char	**ft_args_process(int argc, char **argv, t_stack *stack)
 {
 	int		i;
