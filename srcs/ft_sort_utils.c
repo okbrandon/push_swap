95d961d08fdf5d5a62e2fbf941a9d1c2ac857579
Brandon Soubaigne <bsoubaig@student.42nice.fr>
2022-12-05 15:08:24 +0100

    :rocket: Completed sort utils functions

--- srcs/ft_sort_utils.c
+++ srcs/ft_sort_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_sort_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bsoubaig <bsoubaig@student.42nice.fr>      +#+  +:+       +#+        */
+/*   By: bsoubaig <bsoubaig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 12:13:28 by bsoubaig          #+#    #+#             */
-/*   Updated: 2022/12/05 12:14:53 by bsoubaig         ###   ########.fr       */
+/*   Updated: 2022/12/05 15:07:37 by bsoubaig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,3 +20,18 @@
  *  if it is, it will return 1
  * if it isn't, it will return 0
 */
+int	ft_is_sorted(t_stack *stack)
+{
+	int	i;
+
+	if (!stack || !stack->stack)
+		return (0);
+	i = 0;
+	while (i < stack->size - 1)
+	{
+		if (stack->stack[i] < stack->stack[i + 1])
+			return (0);
+		i++;
+	}
+	return (1);
+}
