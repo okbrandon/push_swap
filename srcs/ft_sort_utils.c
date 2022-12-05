a8848ab0e8fdbc2c7f69868935054eee96683c47
brandonlooksdumb <deustch.private@gmail.com>
2022-12-05 19:35:05 +0100

    :hammer: Added clean comments to sort utils

--- srcs/ft_sort_utils.c
+++ srcs/ft_sort_utils.c
@@ -3,23 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_sort_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bsoubaig <bsoubaig@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bsoubaig <bsoubaig@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 12:13:28 by bsoubaig          #+#    #+#             */
-/*   Updated: 2022/12/05 15:07:37 by bsoubaig         ###   ########.fr       */
+/*   Updated: 2022/12/05 19:34:44 by bsoubaig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/***
- * a function to verify if the stack is sorted
- * it should take a pointer to t_stack
- * it will iterate over the stack's size - 1
- *  and check if the current element is smaller than the next one
- *  if it is, it will return 1
- * if it isn't, it will return 0
-*/
+/**
+ * @brief A function to verify if the stack is sorted
+ * 
+ * @param stack 		pointer to the stack
+ * @return int 			1 if the stack is sorted, 0 if it isn't
+ */
 int	ft_is_sorted(t_stack *stack)
 {
 	int	i;
