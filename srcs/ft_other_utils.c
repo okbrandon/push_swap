11d6f623e2889434a5538e24d8e0c6f79e1ec0a1
brandonlooksdumb <deustch.private@gmail.com>
2022-12-05 19:37:50 +0100

    :hammer: Added clean comments to other utils

--- srcs/ft_other_utils.c
+++ srcs/ft_other_utils.c
@@ -3,18 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_other_utils.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bsoubaig <bsoubaig@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bsoubaig <bsoubaig@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 17:23:48 by bsoubaig          #+#    #+#             */
-/*   Updated: 2022/12/05 14:56:47 by bsoubaig         ###   ########.fr       */
+/*   Updated: 2022/12/05 19:37:33 by bsoubaig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/***
- * A atol function
-*/
+/**
+ * @brief A function to convert a string to a long int
+ * 
+ * @param str 			a long number as a string
+ * @return long			the converted string
+ */
 long	ft_atol(const char *str)
 {
 	int		i;
@@ -41,29 +44,36 @@ long	ft_atol(const char *str)
 	return (result * sign);
 }
 
+/**
+ * @brief A function to free a stack
+ * 
+ * @param stack 		the stack to be freed 
+ */
 void	ft_free_stack(t_stack *stack)
 {
 	free(stack->stack);
 	free(stack);
 }
 
-/***
- * a function to free all stacks
- * it should take two pointers to t_stack
- * and free their stack and the t_stack itself
-*/
+/**
+ * @brief A function to free two stacks
+ * 
+ * @param stack_a 		the first stack to be freed
+ * @param stack_b 		the second stack to be freed
+ */
 void	ft_free_all_stacks(t_stack *stack_a, t_stack *stack_b)
 {
 	ft_free_stack(stack_a);
 	ft_free_stack(stack_b);
 }
 
-/***
- * a ft_error function that takes a char **, a t_stack *, and an int *
- * it should free all the pointers it receives if they are not NULL
- * and then write "Error" to the standard error
- * and exit the program with EXIT_FAILURE
-*/
+/**
+ * @brief A function to exit the program safely
+ * 
+ * @param splitted 		the array of strings to be freed
+ * @param stack 		the stack to be freed
+ * @param array			the array to be freed
+ */
 void	ft_error(char **splitted, t_stack *stack, int *array)
 {
 	int	i;
