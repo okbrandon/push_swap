3d5702487afd3db9a96e5a10cb39388a249c8909
Brandon Soubaigne <bsoubaig@student.42nice.fr>
2022-12-14 09:53:10 +0100

    :rocket: Added simple sorts (two and three int)

--- srcs/ft_sort_process.c
+++ srcs/ft_sort_process.c
@@ -6,29 +6,43 @@
 /*   By: bsoubaig <bsoubaig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 19:39:16 by bsoubaig          #+#    #+#             */
-/*   Updated: 2022/12/13 20:14:20 by bsoubaig         ###   ########.fr       */
+/*   Updated: 2022/12/14 09:49:30 by bsoubaig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-static void	ft_small_sort(t_stack *stack, int type)
+static void	ft_sort_two(t_stack *stack)
 {
 	if (ft_is_sorted(stack))
 		return ;
-	if (type == 2)
+	if (stack->stack[0] > stack->stack[1])
+		ft_do_rotate(stack, 'a');
+	else
+		ft_do_rotate(stack, 'b');
+}
+
+static void	ft_sort_three(t_stack *stack)
+{
+	if (ft_is_sorted(stack))
+		return ;
+	if (stack->stack[0] > stack->stack[1])
 	{
-		if (stack->stack[0] > stack->stack[1])
+		if (stack->stack[1] < stack->stack[stack->size - 1]
+			&& stack->stack[stack->size - 1] < stack->stack[0])
 			ft_do_rotate(stack, 'a');
 		else
-			ft_do_rotate(stack, 'b');
+			ft_do_swap(stack, 'a');
 	}
-	else if (type == 3)
+	if (stack->stack[stack->size - 1] < stack->stack[0]
+		&& stack->stack[0] < stack->stack[1])
+		ft_do_reverse_rotate(stack, 'a');
+	if (stack->stack[0] < stack->stack[stack->size - 1]
+		&& stack->stack[stack->size - 1] < stack->stack[1])
 	{
 		ft_do_swap(stack, 'a');
-		if (stack->stack[0] > stack->stack[stack->size])
-			ft_do_reverse_rotate(stack, 'a');
+		ft_sort_three(stack);
 	}
 }
 
@@ -37,7 +51,7 @@ void	ft_init_sort(t_stack *stack_a, t_stack *stack_b)
 	if (!stack_a || !stack_b)
 		return ;
 	if (stack_a->size == 2)
-		ft_small_sort(stack_a, 2);
+		ft_sort_two(stack_a);
 	if (stack_a->size == 3)
-		ft_small_sort(stack_a, 3);
+		ft_sort_three(stack_a);
 }
--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: bsoubaig <bsoubaig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 13:56:46 by bsoubaig          #+#    #+#             */
-/*   Updated: 2022/12/13 20:15:04 by bsoubaig         ###   ########.fr       */
+/*   Updated: 2022/12/14 09:52:35 by bsoubaig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,27 @@ void	ft_print_stack(t_stack *stack)
 		printf("stack is empty\n");
 		return ;
 	}
-	i = -1;
-	while (++i < stack->size)
-		printf("%d\n", stack->stack[i]);
+	i = 0;
+	while (i < stack->size)
+		printf("%d\n", stack->stack[i++]);
 }
 
+/**
+ * @brief This is a simple main just to test things manually
+ * 
+ * @return int 
+ */
 int	main(void)
 {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
-	int		filled_array[3] = {3, 2, 1};
+	int		filled_array[4] = {1, 3, 2};
 	int		*empty_array = malloc(1);
 
 	stack_a = (t_stack *) malloc(sizeof(stack_a));
 	stack_b = (t_stack *) malloc(sizeof(stack_b));
 	stack_a->stack = filled_array;
+	stack_a->stack[4] = '\0';
 	stack_a->size = 3;
 	stack_a->max_size = 3;
 	stack_b->stack = empty_array;
