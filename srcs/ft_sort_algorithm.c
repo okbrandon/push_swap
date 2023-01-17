92c9dad936beb062e286d8ace41a6a37e8c35da3
Brandon Soubaigne <bsoubaig@student.42nice.fr>
2023-01-17 13:06:42 +0100

    :hammer: Fixed moves being too large for min ints

--- srcs/ft_sort_algorithm.c
+++ srcs/ft_sort_algorithm.c
@@ -6,25 +6,11 @@
 /*   By: bsoubaig <bsoubaig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 20:30:59 by bsoubaig          #+#    #+#             */
-/*   Updated: 2023/01/16 19:13:40 by bsoubaig         ###   ########.fr       */
+/*   Updated: 2023/01/17 12:54:40 by bsoubaig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
-
-int	ft_is_in_chunk(t_stack *stack, int min, int max)
-{
-	int	i;
-
-	i = 0;
-	while (i < stack->size)
-	{
-		if (stack->stack[i] >= min && stack->stack[i] <= max)
-			return (i);
-		i++;
-	}
-	return (-1);
-}
 
 static void	ft_insert_int_to_top(t_stack *stack, int index)
 {
@@ -45,31 +31,63 @@ static void	ft_insert_int_to_top(t_stack *stack, int index)
 	}
 }
 
+static int	ft_find_next_min_int(t_stack *stack, int min)
+{
+	int	i;
+	int	next_min;
+
+	if (!stack || !stack->stack)
+		return (0);
+	i = 0;
+	next_min = INT_MAX;
+	while (i < stack->size)
+	{
+		if (stack->stack[i] > min && stack->stack[i] < next_min)
+			next_min = stack->stack[i];
+		i++;
+	}
+	return (next_min);
+}
+
+static int	ft_get_min_int_index(t_stack *stack, int min)
+{
+	int	i;
+
+	i = 0;
+	while (i < stack->size)
+	{
+		if (stack->stack[i] <= min)
+			break ;
+		i++;
+	}
+	return (i);
+}
+
+#include <stdio.h>
+
 void	ft_chunk_sort(t_stack *stack_a, t_stack *stack_b, int chunk_size)
 {
-	int	from;
-	int	to;
 	int	index;
+	int	min_int;
+	int	times;
 	int	middle;
 
 	middle = ft_get_median(stack_a);
-	from = middle - chunk_size;
-	to = middle + chunk_size;
 	while (stack_a->size)
 	{
-		index = 0;
-		while (index != -1)
+		times = 0;
+		min_int = ft_find_min_int(stack_a);
+		while (times++ < chunk_size)
+			min_int = ft_find_next_min_int(stack_a, min_int);
+		times = 0;
+		while (times++ < chunk_size)
 		{
-			index = ft_is_in_chunk(stack_a, from, to);
-			if (index == -1)
-				break ;
+			index = ft_get_min_int_index(stack_a, min_int);
 			ft_insert_int_to_top(stack_a, index);
 			ft_do_pb(stack_a, stack_b);
 			if (stack_b->stack[0] < middle)
 				ft_do_rotate(stack_b, 'b');
 		}
-		from -= chunk_size;
-		to += chunk_size;
 	}
 	while (stack_b->size)
 	{
